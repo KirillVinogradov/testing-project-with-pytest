37f97f344e4e4c1e13226271b3fef49333f77650
KirillVinogradov <vngradv.k@gmail.com>
2024-01-16 18:55:09 +0300

    Modified MainPage, BasePage and added two test cases for product page.

--- pages/locators.py
+++ pages/locators.py
@@ -1,14 +1,18 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, '#login_link_inc')
+
+class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
 
-class LoginPageLocators():
+class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
 
-class ProductPageLocators():
+class ProductPageLocators:
     ADD_TO_CART_BTN = (By.CSS_SELECTOR, '.btn-add-to-basket')
     PRODUCT_NAME_FROM_NOTICE = (By.CSS_SELECTOR, '.alertinner strong')
     PRODUCT_NAME_FROM_CONTENT = (By.CSS_SELECTOR, '.product_main h1')
--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,7 @@
 import pytest
 from pages.product_page import ProductPage
 from pages.locators import ProductPageLocators
-
+from pages.login_page import LoginPage
 
 @pytest.mark.skip
 @pytest.mark.parametrize('promo', ('promo=offer0', 'promo=offer1', 'promo=offer2', 'promo=offer3', 'promo=offer4',
@@ -51,3 +51,17 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.open()
     page.add_product_to_cart()
     page.success_message_should_be_disappeared()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/'
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/'
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
