7d529de61f1add7d61b8dbd5bc5295ff45b42071
KirillVinogradov <vngradv.k@gmail.com>
2024-01-25 17:21:28 +0300

    Activated headless option.

--- conftest.py
+++ conftest.py
@@ -11,6 +11,8 @@ def pytest_addoption(parser):
 def browser(request):
     language = request.config.getoption('language')
     options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--window-size=1920x1080")
     options.add_experimental_option('prefs', {'intl.accept_languages': language})
     browser = webdriver.Chrome(options=options)
     yield browser
